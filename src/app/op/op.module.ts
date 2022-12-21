24f8500497387caee7d6438ed331eefa07c5c67b
Josue <josuecrda@hotmail.com>
2022-12-21 13:22:27 -0600

    60

--- src/app/op/op.module.ts
+++ src/app/op/op.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './main-page/main-page.component';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -11,6 +12,7 @@ import { MainPageComponent } from './main-page/main-page.component';
   ],
   imports: [
     CommonModule,
+    FormsModule
   ],
   exports: [
     MainPageComponent 
